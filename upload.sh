04b631ff367a43358375b0611bed2a7828eb625d
openwrt <jesstr@mail.ru>
2014-10-21 19:01:20 +0400

    upload.sh: Release hex folder changed to Debug

--- upload.sh
+++ upload.sh
@@ -4,9 +4,9 @@ MK=m8
 PRG=avr910
 BAUD=57600
 PORT=/dev/ttyACM0
-FLASHFILE=./Release/lamp-reminder.hex
+FLASHFILE=./Debug/lamp-reminder.hex
 
 sudo avrdude -p $MK -c $PRG -b $BAUD -P $PORT -U flash:w:$FLASHFILE
 
 # Manual run:
-# sudo avrdude -p m8 -c avr910 -b 57600 -P /dev/ttyACM0 -U flash:w:"./Release/mega8_wifibot.hex":i
+# sudo avrdude -p m8 -c avr910 -b 57600 -P /dev/ttyACM0 -U flash:w:"./Debug/mega8_wifibot.hex":i
